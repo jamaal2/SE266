5f00a1876fa1002e7d5b745f73249a028c592cff
Dublin Jr <jdublinjr@email.neit.edu>
2023-10-23 12:51:30 -0400

    week4

---
+++ week4/teams/models/model_team.php
@@ -0,0 +1,52 @@
+<?php
+
+include (__DIR__ . '/db.php');
+
+    
+    function getTeams(){
+        //grab $db object - 
+        //needs global scope since object is coming from outside the function
+        global $db;
+
+        //initialize return dataset
+        $results = [];
+
+        //prepare our SQL statment
+        $stmt = $db->prepare("SELECT id,teamName, division FROM  teams ORDER BY teamName"); 
+        
+        //if our SQL statement returns results, populate our results array
+        if ( $stmt->execute() && $stmt->rowCount() > 0 ) {
+             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+         }
+         
+         //return results
+         return ($results);
+
+    }
+
+    function addTeam ($teamName, $division) {
+        //grab $db object - 
+        //needs global scope since object is coming from outside the function
+        global $db;
+
+        //initialize return dataset
+        $result = "";
+
+        //prepare our SQL statment
+        $sql = "INSERT INTO teams set teamName = :t, division = :d";
+        $stmt = $db->prepare($sql);
+
+        //bind values
+        $binds = array(
+            ":t" => $teamName,
+            ":d" => $division
+        );
+        
+        //if our SQL statement returns results, populate our results confirmation string
+        if ($stmt->execute($binds) && $stmt->rowCount() > 0) {
+            $result = 'Data Added';
+        }
+        
+        //return results
+        return ($result);
+    }
--- week4/teams/viewteam.php
+++ week4/teams/viewteam.php
@@ -1,5 +1,6 @@
 <?php   
-    require_once __DIR__ . '/controllers/viewController.php';
+    require_once __DIR__ . '/models/model_team.php';
+    $teams = getTeams();
 ?>
 
 
@@ -36,18 +37,12 @@
         <tbody>
 
         <!-- Build each row here -->
-        <?php foreach ($teamListing as $row): ?>
+        <?php foreach ($teams as $row): ?>
             <tr>
-                <td>
-                    <form action="viewTeams.php" method="post">
-                        <input type="hidden" name="teamId" value="<?= $row['id']; ?>" />
-                        <button class="btn glyphicon glyphicon-trash" type="submit"></button>
-                        <?= $row['teamName']; ?>
-                    </form>   
-                </td>
+                <td><?= $row['teamName']; ?></td> 
                 <td><?= $row['division']; ?></td> 
                 <!-- We $_GET the update page with action "Update" -->
-                <td><a href="updateTeam.php?action=Update&teamId=<?= $row['id'] ?>">Update</a></td> 
+                
             </tr>
         <?php endforeach; ?>
         <!-- End table rows -->
